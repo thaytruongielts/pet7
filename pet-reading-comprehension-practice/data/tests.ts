ee4d2a2edbfec731ed2fc59f41066d3fad09d094
thaytruongielts <backuptruong@gmail.com>
2025-11-12 18:16:44 +0700

    Add files via upload

---
+++ pet-reading-comprehension-practice/data/tests.ts
@@ -0,0 +1,82 @@
+
+import { ReadingTest } from '../types';
+
+export const readingTests: ReadingTest[] = [
+  {
+    id: 'computer-game-exhibition',
+    title: 'Computer game exhibition',
+    subtitle: 'B1 English Reading Test',
+    instructions: 'Five sentences have been removed from the text below. For each question, choose the correct answer. There are three extra sentences which you do not need to use.',
+    text: [
+      'Have you ever tried playing the kind of video games that your parents played? The Museum of Science in Manchester, in the UK, has held an exhibition for the last few years, which invites visitors to do exactly that. It offers them the chance to play games from the last 40 years, in various sessions throughout the day.',
+      'These video games sessions have now become one of the main attractions of the museum. They are full of people every day, playing a wide range of games.',
+      '__GAP_1__ For parents, for example, these are usually the games they used to play in their childhood.',
+      'There’s also an educational purpose to the games. For instance, some old types of computer, dating back 40 years, are also available in the sessions. They were originally used in classrooms to teach pupils to write their own computer programs.',
+      '__GAP_2__ Now, the museum is holding workshops that encourage children to learn similar skills – and they’re still very popular.',
+      'The sessions are also seen as social events, as people discover how much fun it is to play video games with other family members. And there’s also an area at Power Up! where a number of visitors can sit down together. __GAP_3__ And nowadays, this is often how fans of video games are more likely to experience playing.',
+      'The exhibition also shows how much progress technology has made over the last 40 years. Parents can often remember playing very simple games. But the games that are played today are more complex. __GAP_4__ And the players also have to use much more complicated techniques.',
+      'However, one serious side of the exhibition is that organisers also want to show that video gaming is an important industry, employing many skilled people.',
+      '__GAP_5__ That way, people who enjoy gaming will also understand all the hard work, talent and imagination that goes into creating these amazing games.'
+    ],
+    options: [
+      { id: 'A', text: 'Visitors each pay for 90-minute sessions.' },
+      { id: 'B', text: 'And at the time, it helped lots of young people to do that.' },
+      { id: 'C', text: 'So they hope the exhibition will share this message.' },
+      { id: 'D', text: 'But not everyone thinks it’s a lot of fun.' },
+      { id: 'E', text: 'However, visitors often choose the ones they’re similar with.' },
+      { id: 'F', text: 'They have better storylines and animation, too.' },
+      { id: 'G', text: 'They also create the music to go with the game.' },
+      { id: 'H', text: 'Then they can all enjoy playing the same game.' }
+    ],
+    correctAnswers: { 1: 'E', 2: 'B', 3: 'H', 4: 'F', 5: 'C' }
+  },
+  {
+    id: 'crime-of-the-century',
+    title: 'The Crime of the Century',
+    subtitle: 'B1 English Reading Test',
+    instructions: 'Five sentences have been removed from the text below. For each question, choose the correct answer. There are three extra sentences which you do not need to use.',
+    text: [
+        'When the Mona Lisa was stolen from the Louvre in Paris in 1911 it caused a sensation and left the art world and police puzzled as to how it had been taken and who had carried out the crime. It also helped make the work of art probably the most famous painting in the world.',
+        'At first, the police suspected artists who didn’t like classical works of art of carrying out the crime. The poet Apollinaire was arrested and questioned for a few days. __GAP_1__',
+        'However, the true thief was an Italian man named Vincenzo Peruggia. He had a criminal record and had found work in the Louvre for a time. On the day the crime took place, he entered the museum along with other museum employees wearing the same white smock. __GAP_2__ He then took the painting off the wall and left the building with the work of art wrapped in his white smock.',
+        'Peruggia kept the painting for two years in his Paris apartment before eventually contacting an Italian art dealer, offering to return the painting to Florence for a reward. __GAP_3__ He argued he was returning the painting to its rightful home.',
+        'He was finally arrested after passing the painting to the dealer. However, many Italians saw his actions as those of a national hero. __GAP_4__ The painting was indeed shown off around Italy before it was eventually returned to Paris and the Louvre.',
+        '__GAP_5__ However, after being stolen and finally returned, it became more well known, its popularity grew and, as we know now, made the Mona Lisa smile famous around the world.'
+    ],
+    options: [
+        { id: 'A', text: 'Previously, the painting had been admired by experts.' },
+        { id: 'B', text: 'He waited until there was nobody in the gallery.' },
+        { id: 'C', text: 'So the painting became popular in France.' },
+        { id: 'D', text: 'Because of this, he only went to prison for a short while.' },
+        { id: 'E', text: 'He said that he was doing his duty as a proud Italian.' },
+        { id: 'F', text: 'But nobody knew it had disappeared.' },
+        { id: 'G', text: 'Even the great Pablo Picasso was thought by some to be guilty.' },
+        { id: 'H', text: 'It is now kept safely behind special glass.' }
+    ],
+    correctAnswers: { 1: 'G', 2: 'B', 3: 'E', 4: 'D', 5: 'A' }
+  },
+  {
+    id: 'my-first-marathon',
+    title: 'My first marathon',
+    subtitle: 'B1 English Reading Test',
+    instructions: 'Five sentences have been removed from the text below. For each question, choose the correct answer. There are three extra sentences which you do not need to use.',
+    text: [
+        'The alarm went off at half-past four that morning. It was far too early to get up, and the idea of running 26 miles seemed impossible. __GAP_1__ I pulled myself out of bed, showered and pulled on my running gear.',
+        'By six o’clock, I was at the Dodger Stadium, where the rest of my running group was already waiting. When they saw the look of terror on my face, they laughed kindly. This was clearly my first marathon. __GAP_2__ When I realized that I was just one of them, I felt a little better. Helicopters hovered overhead ready to film us as we ran through the streets of Los Angeles, passing every major landmark until we reached the sea at Santa Monica. Suddenly, I felt very lucky and proud to be a part of it. __GAP_3__',
+        'As I began to run, I tried to ignore the aches in my body from eight months of training. Instead, I put one foot in front of the other and kept going. By mile five, my body was complaining, and by mile eight, my muscles were screaming at me.',
+        '__GAP_4__ I was running alone now, but I was still running. By mile 12, I was ready to give up, and by mile 15, I was moving even more slowly. By mile 18, I was in my neighbourhood. __GAP_5__ But by mile 22, there were only four miles to go. Spectators yelled encouragement at me as I ran past. ‘You can do it! Keep going!’',
+        'As the finish line came into sight, I started running faster and faster, and I crossed that line running. I grinned as the medal was placed over my head, and then I cried. I had done it. I had run my first marathon!'
+    ],
+    options: [
+        { id: 'A', text: 'After eight months of training, this was going to be easy.' },
+        { id: 'B', text: 'I slowed down, and the rest of my group moved away.' },
+        { id: 'C', text: 'But I had trained for this, and I was going to do it.' },
+        { id: 'D', text: 'There was no way I could finish the race now.' },
+        { id: 'E', text: 'The horn blew to start the race.' },
+        { id: 'F', text: 'I was very tempted to turn down my street and go home.' },
+        { id: 'G', text: 'I was so glad that I had decided to do this.' },
+        { id: 'H', text: 'Thousands of runners from all over the world were gathered for the race.' }
+    ],
+    correctAnswers: { 1: 'C', 2: 'H', 3: 'G', 4: 'B', 5: 'F' }
+  }
+];
